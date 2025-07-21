96510556bdf390f8dc64afa48c1a0b9deb4e1321
Sifat Hossain <sifathossain1326@gmail.com>
2025-07-22 01:30:28 +0530

    Final Project

--- js/index.js
+++ js/index.js
@@ -13,6 +13,22 @@ document.getElementById('btn-donation-1').addEventListener('click', function(){
         document.getElementById('text-value-1').innerText = inputValue1 + textValue1;
         document.getElementById('text-value-main').innerText = textValueMain - inputValue1;
         document.getElementById('input-value-1').value = '';
+
+
+        // Add History Section;
+
+        const div = document.createElement('div');
+        div.classList.add('border');
+        div.classList.add('rounded-lg');
+        div.classList.add('p-4');
+        div.classList.add('mb-4');
+        const d = new Date();
+
+        div.innerHTML = `
+            <h3 class = "text-xl font-bold mb-4"> ${inputValue1 + textValue1} Tk Donated for Flood at Noakhali, Bangladesh </h4>
+            <p class = "text-gray-500"> ${d} </>
+        `
+        document.getElementById('history').appendChild(div);
     }
     else{
         alert('Your have not enough money in your account.')
@@ -26,13 +42,28 @@ document.getElementById('btn-donation-2').addEventListener('click', function(){
     const textValue2 = getTextValue('text-value-2');
 
     const textValueMain = getTextValue('text-value-main');
-    console.log(inputValue2, textValue2, textValueMain);
+    // console.log(inputValue2, textValue2, textValueMain);
     
     if(textValueMain >= inputValue2){
         document.getElementById('text-value-2').innerText = textValue2 + inputValue2;
         document.getElementById('text-value-main').innerText = textValueMain - inputValue2;
 
         document.getElementById('input-value-2').value = '';
+
+        // Add History Section;
+
+        const div = document.createElement('div');
+        div.classList.add('border');
+        div.classList.add('rounded-lg');
+        div.classList.add('p-4');
+        div.classList.add('mb-4');
+        const d = new Date();
+
+        div.innerHTML = `
+            <h3 class = "text-xl font-bold mb-4"> ${inputValue2 + textValue2} Tk Donated for Flood Relief in Feni,Bangladesh </h4>
+            <p class = "text-gray-500"> ${d} </>
+        `
+        document.getElementById('history').appendChild(div);
     }
     else{
         alert('Your have not enough money in your account.');
@@ -46,13 +77,28 @@ document.getElementById('btn-donation-3').addEventListener('click', function(){
     const textValue3 = getTextValue('text-value-3');
 
     const textValueMain = getTextValue('text-value-main');
-    console.log(inputValue3, textValue3, textValueMain);
+    // console.log(inputValue3, textValue3, textValueMain);
     
     if(textValueMain >= inputValue3){
         document.getElementById('text-value-3').innerText = textValue3 + inputValue3;
         document.getElementById('text-value-main').innerText = textValueMain - inputValue3;
 
         document.getElementById('input-value-3').value = '';
+
+        // Add History Section;
+
+        const div = document.createElement('div');
+        div.classList.add('border');
+        div.classList.add('rounded-lg');
+        div.classList.add('p-4');
+        div.classList.add('mb-4');
+        const d = new Date();
+
+        div.innerHTML = `
+            <h3 class = "text-xl font-bold mb-4"> ${inputValue3 + textValue3} Tk Donated for Injured in the Quota Movement </h4>
+            <p class = "text-gray-500"> ${d} </>
+        `
+        document.getElementById('history').appendChild(div);
     }
     else{
         alert('Your have not enough money in your account.');
